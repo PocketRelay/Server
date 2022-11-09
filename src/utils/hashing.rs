7fad126ac7dfa504bb6da3fccde854e3255866e2
Jacobtread <jacobtread@gmail.com>
2022-11-09 20:59:59 +1300

    Test password hashing and documented

--- src/utils/hashing.rs
+++ src/utils/hashing.rs
@@ -3,6 +3,10 @@ use argon2::{Argon2, PasswordHash, PasswordHasher};
 use password_hash::PasswordVerifier;
 use rand_core::OsRng;
 
+/// Hashes the provided password using the Argon2 algorithm returning
+/// the generated hash in string form.
+///
+/// `password` The password to hash
 pub fn hash_password(password: &str) -> password_hash::Result<String> {
     let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
@@ -11,6 +15,11 @@ pub fn hash_password(password: &str) -> password_hash::Result<String> {
     Ok(value)
 }
 
+/// Verifies the hash of the provided password checking that
+/// it matches the provided hash
+///
+/// `password` The plain text password
+/// `hash`     The hashed password
 pub fn verify_password(password: &str, hash: &str) -> bool {
     let hash = match PasswordHash::new(hash) {
         Ok(value) => value,
@@ -18,4 +27,20 @@ pub fn verify_password(password: &str, hash: &str) -> bool {
     };
     let argon2 = Argon2::default();
     argon2.verify_password(password.as_bytes(), &hash).is_ok()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::utils::generate_token;
+
+    use super::{hash_password, verify_password};
+
+    /// Tests that password hashing works correctly
+    #[test]
+    fn test_password_hashing() {
+        let value = generate_token(50);
+        let hash = hash_password(&value).unwrap();
+        let valid = verify_password(&value, &hash);
+        assert_eq!(valid, true)
+    }
 }
