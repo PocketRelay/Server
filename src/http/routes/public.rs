63445494d0f22b4cf5d97b4918520fcfa4890d77
Jacobtread <jacobtread@gmail.com>
2022-11-09 21:17:43 +1300

    Fixed content loading

--- src/http/routes/public.rs
+++ src/http/routes/public.rs
@@ -14,13 +14,15 @@ pub fn configure(cfg: &mut ServiceConfig) {
 #[get("/content/{filename:.*}")]
 async fn content(path: web::Path<String>) -> impl Responder {
     let path = path.into_inner();
+    let path = path.split("/").last();
+
+    let Some(path) = path else {
+        return HttpResponse::NotFound().body("Not Found");
+    };
+
     if let Some(file) = PublicContent::get(&path) {
         HttpResponse::Ok()
-            .content_type(
-                mime_guess::from_path(&path)
-                    .first_or_octet_stream()
-                    .as_ref(),
-            )
+            .content_type(mime_guess::from_path(&path).first_or_text_plain().as_ref())
             .body(file.data.into_owned())
     } else {
         HttpResponse::NotFound().body("Not Found")
