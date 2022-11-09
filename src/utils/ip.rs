8c7c5057cb393d254f7817435d8ad4aa7bf2e99a
Jacobtread <jacobtread@gmail.com>
2022-11-09 20:56:49 +1300

    Test for public address

--- src/utils/ip.rs
+++ src/utils/ip.rs
@@ -11,3 +11,24 @@ pub async fn public_address() -> Option<String> {
     let result = result.replace("\n", "");
     Some(result)
 }
+
+#[cfg(test)]
+mod test {
+    use super::public_address;
+
+    /// Test function for ensuring that the public address returned
+    /// from `public_address` is actually an IPv4 address
+    #[tokio::test]
+    async fn test_public_address() {
+        let value = public_address()
+            .await
+            .expect("Failed to retriever public address");
+        let parts = value
+            .split(".")
+            .filter_map(|value| value.parse::<u32>().ok())
+            .collect::<Vec<u32>>();
+
+        println!("Public address was: {}", value);
+        assert!(parts.len() == 4)
+    }
+}
