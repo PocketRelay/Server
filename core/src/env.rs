95a8c13de7fa28427a9626d445b74374f7736102
Jacobtread <jacobtread@gmail.com>
2022-11-23 16:18:24 +1300

    Spaced env vars

--- core/src/env.rs
+++ core/src/env.rs
@@ -10,17 +10,23 @@ pub const EXT_HOST: (&str, &str) = ("PR_EXT_HOST", "gosredirector.ea.com");
 pub const REDIRECTOR_PORT: (&str, u16) = ("PR_REDIRECTOR_PORT", 42127);
 pub const MAIN_PORT: (&str, u16) = ("PR_MAIN_PORT", 14219);
 pub const HTTP_PORT: (&str, u16) = ("PR_HTTP_PORT", 80);
+
 pub const MENU_MESSAGE: (&str, &str) = (
     "PR_MENU_MESSAGE",
     "<font color='#B2B2B2'>Pocket Relay</font> - <font color='#FFFF66'>Logged as: {n}</font>",
 );
+
 pub const DATABASE_FILE: (&str, &str) = ("PR_DATABASE_FILE", "data/app.db");
 pub const DATABASE_URL: &str = "PR_DATABASE_URL";
+
 pub const GAW_DAILY_DECAY: (&str, f32) = ("PR_GAW_DAILY_DECAY", 0.0);
 pub const GAW_PROMOTIONS: (&str, bool) = ("PR_GAW_PROMOTIONS", true);
+
 pub const RETRIEVER: (&str, bool) = ("PR_RETRIEVER", true);
+
 pub const ORIGIN_FETCH: (&str, bool) = ("PR_ORIGIN_FETCH", true);
 pub const ORIGIN_FETCH_DATA: (&str, bool) = ("PR_ORIGIN_FETCH_DATA", true);
+
 pub const MITM_ENABLED: (&str, bool) = ("PR_MITM_ENABLED", false);
 
 pub const LOGGING_LEVEL: (&str, LevelFilter) = ("PR_LOG_LEVEL", LevelFilter::Info);
