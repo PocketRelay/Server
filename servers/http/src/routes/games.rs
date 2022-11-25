45517cd2cdef7cd949f5d14c3be223e617cdfc7a
Jacobtread <jacobtread@gmail.com>
2022-11-25 17:06:05 +1300

    Cleaned up and documented Games api

--- servers/http/src/routes/games.rs
+++ servers/http/src/routes/games.rs
@@ -10,47 +10,51 @@ use actix_web::{
     web::{Json, Path, ServiceConfig},
     ResponseError,
 };
-use serde::Serialize;
+
 use utils::types::GameID;
 
 /// Function for configuring the services in this route
+///
+/// `cfg` Service config to configure
 pub fn configure(cfg: &mut ServiceConfig) {
-    cfg.service(games_list).service(game);
+    cfg.service(get_games).service(get_game);
 }
 
-#[derive(Debug, Serialize)]
-pub enum GamesError {
-    GameNotFound,
-}
+/// Error type for a game that couldn't be located
+#[derive(Debug)]
+struct GameNotFound;
 
-impl Display for GamesError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::GameNotFound => f.write_str("Couldn't find any games with that ID"),
-        }
-    }
-}
-
-impl ResponseError for GamesError {
-    fn status_code(&self) -> actix_web::http::StatusCode {
-        match self {
-            Self::GameNotFound => StatusCode::NOT_FOUND,
-        }
-    }
-}
-
+/// Route for retrieving a list of all the games that are currently running.
+/// Will take a snapshot of all the games.
 #[get("/api/games")]
-async fn games_list() -> Json<GamesSnapshot> {
+async fn get_games() -> Json<GamesSnapshot> {
     let games = GlobalState::games().snapshot().await;
     Json(games)
 }
 
+/// Route for retrieving the details of a game with a specific game ID
+///
+/// `game_id` The ID of the game
 #[get("/api/games/{id}")]
-async fn game(path: Path<GameID>) -> Result<Json<GameSnapshot>, GamesError> {
-    let game_id = path.into_inner();
+async fn get_game(game_id: Path<GameID>) -> Result<Json<GameSnapshot>, GameNotFound> {
     let games = GlobalState::games()
-        .snapshot_id(game_id)
+        .snapshot_id(game_id.into_inner())
         .await
-        .ok_or(GamesError::GameNotFound)?;
+        .ok_or(GameNotFound)?;
     Ok(Json(games))
+}
+
+/// Display for game not found just an error message saying it couldn't
+/// find any games with that ID
+impl Display for GameNotFound {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Game with that ID not found")
+    }
+}
+
+/// Game not found responses are always 404 errors
+impl ResponseError for GameNotFound {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::NOT_FOUND
+    }
 }
