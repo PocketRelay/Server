fc53decf7884214fb3460ecdd9e2232a117fca1e
Jacobtread <jacobtread@gmail.com>
2022-11-25 17:43:03 +1300

    Log connection urls on startup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use log::info;
 use tokio::join;
+use utils::net::public_address;
 
 use core::{constants, env, state::GlobalState};
 
@@ -22,6 +23,8 @@ async fn main() {
 
     info!("Starting Pocket Relay v{}", constants::VERSION);
 
+    log_connection_urls().await;
+
     // Initialize global state
     GlobalState::init().await;
 
@@ -39,4 +42,43 @@ async fn main() {
             main_server::start_server()
         );
     }
+}
+
+/// Prints a list of possible urls that can be used to connect to
+/// this Pocket relay server
+async fn log_connection_urls() {
+    let http_port = env::from_env(env::HTTP_PORT);
+    let mut output = String::new();
+    if let Ok(local_address) = local_ip_address::local_ip() {
+        output.push_str("LAN: ");
+        output.push_str(&local_address.to_string());
+        if http_port != 80 {
+            output.push(':');
+            output.push_str(&http_port.to_string());
+        }
+    }
+    if let Some(public_address) = public_address().await {
+        if output.len() > 0 {
+            output.push_str(", ");
+        }
+
+        output.push_str("WAN: ");
+        output.push_str(&public_address.to_string());
+        if http_port != 80 {
+            output.push(':');
+            output.push_str(&http_port.to_string());
+        }
+    }
+
+    if output.len() > 0 {
+        output.push_str(", ");
+    }
+
+    output.push_str("LOCAL: 127.0.0.1");
+    if http_port != 80 {
+        output.push(':');
+        output.push_str(&http_port.to_string());
+    }
+
+    info!("Connection URLS ({output})");
 }
