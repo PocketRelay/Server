945dbc4b68e1cc41f6e091a4558b5b4d61450006
Jacobtread <jacobtread@gmail.com>
2022-12-20 19:41:29 +1300

    Cors fixes

--- src/servers/http/middleware/cors.rs
+++ src/servers/http/middleware/cors.rs
@@ -3,6 +3,7 @@ use axum::{
     middleware::Next,
     response::Response,
 };
+use reqwest::StatusCode;
 
 /// Middleware layer function for appending CORS headers to requests
 /// and responding to options requests
@@ -13,12 +14,26 @@ pub async fn cors_layer<T>(req: Request<T>, next: Next<T>) -> Response {
     // Create a new response for OPTIONS requests
     let mut res: Response = if req.method() == Method::OPTIONS {
         // Default response for OPTIONS requests
-        Response::default()
+        let mut res = Response::default();
+        *res.status_mut() = StatusCode::NO_CONTENT;
+        let headers = res.headers_mut();
+        headers.insert(
+            header::ACCESS_CONTROL_ALLOW_METHODS,
+            HeaderValue::from_static("*"),
+        );
+        headers.insert(
+            header::ACCESS_CONTROL_ALLOW_HEADERS,
+            HeaderValue::from_static("*"),
+        );
+        res
     } else {
         next.run(req).await
     };
+
+    let headers = res.headers_mut();
+
     // Append access control allow origin header for all origins
-    res.headers_mut().insert(
+    headers.insert(
         header::ACCESS_CONTROL_ALLOW_ORIGIN,
         HeaderValue::from_static("*"),
     );
