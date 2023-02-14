91d8aff1320a19e91454ebe34a968cfcef0b284c
Jacobtread <jacobtread@gmail.com>
2023-02-14 17:25:55 +1300

    Fixed removed wrong old fn

--- src/services/game/player.rs
+++ src/services/game/player.rs
@@ -69,21 +69,25 @@ impl GamePlayer {
         )
     }
 
-    pub fn encode(&self, slot: usize, writer: &mut TdfWriter) {
-        writer.tag_empty_blob(b"BLOB");
-        writer.tag_u8(b"EXID", 0);
-        writer.tag_u32(b"GID", self.game_id);
-        writer.tag_u32(b"LOC", 0x64654445);
-        writer.tag_str(b"NAME", &self.player.display_name);
-        writer.tag_u32(b"PID", self.player.id);
-        self.net.tag_groups(b"PNET", writer);
-        writer.tag_usize(b"SID", slot);
-        writer.tag_u8(b"SLOT", 0);
-        writer.tag_value(b"STAT", &self.state);
-        writer.tag_u16(b"TIDX", 0xffff);
-        writer.tag_u8(b"TIME", 0);
-        writer.tag_triple(b"UGID", (0, 0, 0));
-        writer.tag_u32(b"UID", self.addr.id);
+    pub fn encode_data(&self, writer: &mut TdfWriter) {
+        self.net.tag_groups(b"ADDR", writer);
+        writer.tag_str(b"BPS", "ea-sjc");
+        writer.tag_str_empty(b"CTY");
+        writer.tag_var_int_list_empty(b"CVAR");
+        {
+            writer.tag_map_start(b"DMAP", TdfType::VarInt, TdfType::VarInt, 1);
+            writer.write_u32(0x70001);
+            writer.write_u16(0x409a);
+        }
+        writer.tag_u16(b"HWFG", self.net.hardware_flags);
+        {
+            writer.tag_list_start(b"PSLM", TdfType::VarInt, 1);
+            writer.write_u32(0xfff0fff);
+        }
+        writer.tag_value(b"QDAT", &self.net.qos);
+        writer.tag_u8(b"UATT", 0);
+        writer.tag_list_start(b"ULST", TdfType::Triple, 1);
+        (4, 1, self.game_id).encode(writer);
         writer.tag_group_end();
     }
 }
