bf451172ae3c75d47c490d8199bf7a8cd7073a76
Jacobtread <jacobtread@gmail.com>
2023-03-14 16:58:58 +1300

    Doc updates

--- src/utils/random.rs
+++ src/utils/random.rs
@@ -1,16 +1,19 @@
 use argon2::password_hash::rand_core::{OsRng, RngCore};
 
-pub fn random_string(len: usize) -> String {
+/// Creates a random string of the provided length
+///
+/// `length` The length of the random string
+pub fn random_string(length: usize) -> String {
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
     abcdefghijklmnopqrstuvwxyz\
     0123456789";
     const RANGE: usize = CHARSET.len();
 
     let mut rand = OsRng;
-    let mut output = String::with_capacity(len);
+    let mut output = String::with_capacity(length);
 
     // Loop until the string length is finished
-    for _ in 0..len {
+    for _ in 0..length {
         // Loop until a valid random is found
         loop {
             let var = (rand.next_u32() >> (32 - 6)) as usize;
