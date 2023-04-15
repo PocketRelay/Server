e24edd168f1757d4340d659a0bd7e70f424aeff1
Jacobtread <jacobtread@gmail.com>
2023-04-15 16:32:41 +1200

    Removed unnessicary caching and local ip from public address fetching

--- src/utils/net.rs
+++ src/utils/net.rs
@@ -1,51 +1,13 @@
-use std::{
-    net::{IpAddr, Ipv4Addr},
-    time::{Duration, SystemTime},
-};
-
 use reqwest;
 use serde::Deserialize;
-use tokio::sync::RwLock;
-
-/// Caching structure for the public address value
-enum PublicAddrCache {
-    /// The value hasn't yet been computed
-    Unset,
-    /// The value has been computed
-    Set {
-        /// The public address value
-        value: Ipv4Addr,
-        /// The system time the cache expires at
-        expires: SystemTime,
-    },
-}
-
-/// Cache value for storing the public address
-static PUBLIC_ADDR_CACHE: RwLock<PublicAddrCache> = RwLock::const_new(PublicAddrCache::Unset);
-
-/// Cache public address for 2 hours
-const ADDR_CACHE_TIME: Duration = Duration::from_secs(60 * 60 * 2);
+use std::net::Ipv4Addr;
 
 /// Retrieves the public address of the server either using the cached
 /// value if its not expired or fetching the new value from the API using
 /// `fetch_public_addr`
 pub async fn public_address() -> Option<Ipv4Addr> {
-    {
-        let cached = &*PUBLIC_ADDR_CACHE.read().await;
-        if let PublicAddrCache::Set { value, expires } = cached {
-            let time = SystemTime::now();
-            if time.lt(expires) {
-                return Some(*value);
-            }
-        }
-    }
-
-    // Hold the write lock to prevent others from attempting to update aswell
-    let cached = &mut *PUBLIC_ADDR_CACHE.write().await;
-
     // API addresses for IP lookup
     let addresses = ["https://api.ipify.org/", "https://ipv4.icanhazip.com/"];
-    let mut value: Option<Ipv4Addr> = None;
 
     // Try all addresses using the first valid value
     for address in addresses {
@@ -60,29 +22,11 @@ pub async fn public_address() -> Option<Ipv4Addr> {
         };
 
         if let Ok(parsed) = ip.parse() {
-            value = Some(parsed);
-            break;
+            return Some(parsed);
         }
     }
 
-    // If we couldn't connect to any IP services its likely
-    // we don't have internet lets try using our local address
-    if value.is_none() {
-        if let Ok(IpAddr::V4(addr)) = local_ip_address::local_ip() {
-            value = Some(addr)
-        }
-    }
-
-    let value = value?;
-
-    // Update cached value with the new address
-
-    *cached = PublicAddrCache::Set {
-        value,
-        expires: SystemTime::now() + ADDR_CACHE_TIME,
-    };
-
-    Some(value)
+    None
 }
 
 /// Structure for the lookup responses from the google DNS API
