75329f7ff95604e71fb7bfb3b859ccaa3e91b987
Jacobtread <jacobtread@gmail.com>
2024-07-02 16:58:14 +1200

    feat: update entitlement list

--- src/session/routes/auth.rs
+++ src/session/routes/auth.rs
@@ -146,51 +146,57 @@ pub async fn handle_logout(
 
 // Skip formatting these entitlement creations
 #[rustfmt::skip]
-static ENTITLEMENTS: &[Entitlement; 34] = &[
+static ENTITLEMENTS: &[Entitlement] = &[
     // Project 10 = $10 Multiplayer Pass (Entitlement Required For Online Access)
     Entitlement::new_pc(0xec5090c43f,"303107",2,"DR:229644400","PROJECT10_CODE_CONSUMED",1),
     Entitlement::new_pc(0xec3e4d793f,"304141",2,"DR:230773600","PROJECT10_CODE_CONSUMED_LE1",1),
     Entitlement::new_pc(0xec3e4d793f,"304141",2,"DR:230773600","PROJECT10_CODE_CONSUMED_LE1",1),
+    Entitlement::new_pc(1015818446559,"304141",2,"Origin.OFR.50.0005237","PROJECT10_CODE_CONSUMED_LE1",1),
+
     // Jeeze so many online pass entitlements
-    Entitlement::new_pc(0xec50b255ff,"300241",2,"OFB-MASS:44370","ONLINE_ACCESS",1),
-    Entitlement::new_pc(0xec50a620ff,"300241",2,"OFB-MASS:49465","ONLINE_ACCESS",1),
+    Entitlement::new_pc(1015819446559,"300241",2,"OFB-MASS:44370","ONLINE_ACCESS",1),
+    Entitlement::new_pc(1015788546559,"300241",2,"OFB-MASS:49465","ONLINE_ACCESS",1),
     Entitlement::new_pc(0xec508db6ff,"303107",2,"DR:229644400","ONLINE_ACCESS",1),
     Entitlement::new_pc(0xec3e5393bf,"300241",2,"OFB-EAST:46112","ONLINE_ACCESS",1),
     Entitlement::new_pc(0xec3e50867f,"304141",2,"DR:230773600","ONLINE_ACCESS",1),
+    Entitlement::new_pc(1015818846559,"300241",2,"Origin.OFR.50.0002448","ONLINE_ACCESS",1),
+    Entitlement::new_pc(1015818646559,"303107",2,"Origin.OFR.50.0005237","ONLINE_ACCESS",1),
+
+    Entitlement::new_gen(1017854546559,"303107", 0,"","ONLINE_ACCESS_GAW_WEB",1),
     Entitlement::new_gen(0xec4495bfff,"303107", 0,"","ONLINE_ACCESS_GAW_PC",1),
     Entitlement::new_gen(0xea234c3e7f,"303107", 2,"","ONLINE_ACCESS_GAW_XBL2",1),
     // Singleplayer DLC
-    Entitlement::new_pc(0xec3e62d5ff,"300241",2,"OFB-MASS:51074","ME3_PRC_EXTENDEDCUT",5),
-    Entitlement::new_pc(0xec50b5633f,"300241",2,"OFB-MASS:44370","ME3_PRC_PROTHEAN",5),
+    Entitlement::new_pc(1015789146559,"300241",2,"OFB-MASS:51074","ME3_PRC_EXTENDEDCUT",5),
+    Entitlement::new_pc(1015819246559,"300241",2,"OFB-MASS:44370","ME3_PRC_PROTHEAN",5),
     Entitlement::new_pc(0xec3e56a0ff,"300241",2,"OFB-EAST:46112","ME3_PRC_PROTHEAN",5),
-    Entitlement::new_pc(0xec50b8707f,"300241",2,"OFB-MASS:52001","ME3_PRC_LEVIATHAN",5),
-    Entitlement::new_pc(0xec50ac3b7f,"300241",2,"OFB-MASS:55146","ME3_PRC_OMEGA",5),
+    Entitlement::new_pc(1015789546559,"300241",2,"OFB-MASS:52001","ME3_PRC_LEVIATHAN",5),
+    Entitlement::new_pc(1015789746559,"300241",2,"OFB-MASS:55146","ME3_PRC_OMEGA",5),
     Entitlement::new_pc(0xec5093d17f,"300241",2,"OFB-EAST:58040","MET_BONUS_CONTENT_DW",5),
-    Entitlement::new_pc(0xec50af48bf,"300241",2,"OFB-EAST:57550","ME3_PRC_CITADEL",5),
+    Entitlement::new_pc(1015823046559,"300241",2,"OFB-EAST:57550","ME3_PRC_CITADEL",5),
     // Singleplayer Packs
-    Entitlement::new_pc(0xec50bb7dbf,"300241",2,"OFB-MASS:56984","ME3_MTX_APP01",5),
-    Entitlement::new_pc(0xec5099ebff,"300241",2,"OFB-MASS:49032","ME3_MTX_GUN01",5),
-    Entitlement::new_pc(0xec50c1983f,"300241",2,"OFB-MASS:55147","ME3_MTX_GUN02",5),
+    Entitlement::new_pc(1015790146559,"300241",2,"OFB-MASS:56984","ME3_MTX_APP01",5),
+    Entitlement::new_pc(1015821046559,"300241",2,"OFB-MASS:49032","ME3_MTX_GUN01",5),
+    Entitlement::new_pc(1015789946559,"300241",2,"OFB-MASS:55147","ME3_MTX_GUN02",5),
     // Multiplayer DLC
-    Entitlement::new_pc(0xec50a0067f,"300241",2,"OFB-MASS:47872","ME3_PRC_RESURGENCE",5),
-    Entitlement::new_pc(0xec50a92e3f,"300241",2,"OFB-MASS:49465","ME3_PRC_REBELLION",5),
-    Entitlement::new_pc(0xec5096debf,"300241",2,"OFB-MASS:51073","ME3_PRC_EARTH",5),
-    Entitlement::new_pc(0xec509cf93f,"300241",2,"OFB-MASS:52000","ME3_PRC_GOBIG",5),
-    Entitlement::new_pc(0xec50a313bf,"300241",2,"OFB-MASS:59712","ME3_PRC_MP5",5),
+    Entitlement::new_pc(1015820846559,"300241",2,"OFB-MASS:47872","ME3_PRC_RESURGENCE",5),
+    Entitlement::new_pc(1015788746559,"300241",2,"OFB-MASS:49465","ME3_PRC_REBELLION",5),
+    Entitlement::new_pc(1015788946559,"300241",2,"OFB-MASS:51073","ME3_PRC_EARTH",5),
+    Entitlement::new_pc(1015789346559,"300241",2,"OFB-MASS:52000","ME3_PRC_GOBIG",5),
+    Entitlement::new_pc(1015790546559,"300241",2,"OFB-MASS:59712","ME3_PRC_MP5",5),
     // Other
-    Entitlement::new_pc(0xec81ae023f,"300241",2,"OFB-MASS:46111","ME3_PRO_M90_INDRA",5),
-    Entitlement::new_pc(0xec81aaf4ff,"300241",2,"OFB-MASS:46110","ME3_PRO_AT12_RAIDER_PACK",5),
-    Entitlement::new_pc(0xec81a7e7bf,"300241",2,"OFB-MASS:46033","ME3_PRO_M55_ARGUS",5),
-    Entitlement::new_pc(0xec81a4da7f,"300241",2,"OFB-MASS:46032","ME3_PRO_N7_WARFARE_PACK",5),
-    Entitlement::new_pc(0xec81a1cd3f,"300241",2,"OFB-MASS:46489","ME3_PRO_N7_WARFARE_PACK",5),
+    Entitlement::new_pc(1015820646559,"300241",2,"OFB-MASS:46111","ME3_PRO_M90_INDRA",5),
+    Entitlement::new_pc(1015820446559,"300241",2,"OFB-MASS:46110","ME3_PRO_AT12_RAIDER_PACK",5),
+    Entitlement::new_pc(1015820246559,"300241",2,"OFB-MASS:46033","ME3_PRO_M55_ARGUS",5),
+    Entitlement::new_pc(1015820046559,"300241",2,"OFB-MASS:46032","ME3_PRO_N7_WARFARE_PACK",5),
+    Entitlement::new_pc(1015819846559,"300241",2,"OFB-MASS:46489","ME3_PRO_N7_WARFARE_PACK",5),
+    Entitlement::new_pc(1015819046559,"300241",2,"Origin.OFR.50.0002448","ORIGIN_BUNDLE_OWNED",5),
 
-    
     // Collectors Edition
-    Entitlement::new_pc(0xec3e5fc8bf,"300241",2,"OFB-MASS:46484","ME3_MTX_COLLECTORS_EDITION",5),
+    Entitlement::new_pc(1015819646559,"300241",2,"OFB-MASS:46484","ME3_MTX_COLLECTORS_EDITION",5),
     Entitlement::new_pc(0xec3e5cbb7f,"300241",2,"OFB-MASS:46483","ME3_MTX_DIGITAL_ART_BOOKS",5),
     Entitlement::new_gen(0xec3e59ae3f,"300241",2,"OFB-MASS:46482","ME3_MTX_SOUNDTRACK",5),
     // Darkhorse Redeem Code (Character boosters and Collector Assault Rifle)
-    Entitlement::new_pc(0xec50be8aff,"300241",2,"OFB-MASS:61524","ME3_PRC_DARKHORSECOMIC",5),
+    Entitlement::new_pc(1015790746559,"300241",2,"OFB-MASS:61524","ME3_PRC_DARKHORSECOMIC",5),
 ];
 
 /// Handles list user entitlements 2 responses requests which contains information
