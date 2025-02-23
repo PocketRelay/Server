bbc0bcfa39527e1d010c518a6d67722aba2e5cbf
Jacobtread <jacobtread@gmail.com>
2025-02-23 21:19:32 +1300

    refactor: optimize udp tunnel to use a single future removing any shared access to the socket

--- src/services/tunnel/udp_tunnel.rs
+++ src/services/tunnel/udp_tunnel.rs
@@ -1,8 +1,12 @@
 use crate::services::sessions::Sessions;
-use futures_util::{stream::FuturesUnordered, Stream};
 use log::{debug, error};
 use pocket_relay_udp_tunnel::{deserialize_message, serialize_message, TunnelMessage};
-use std::{future::poll_fn, net::SocketAddr, sync::Arc, task::Poll};
+use std::collections::VecDeque;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::ready;
+use std::{net::SocketAddr, sync::Arc, task::Poll};
+use tokio::io::ReadBuf;
 use tokio::{net::UdpSocket, time::Instant};
 
 use super::TunnelService;
@@ -33,66 +37,44 @@ pub async fn start_udp_tunnel(
 
     debug!("started tunneling server {tunnel_addr}");
 
-    tokio::spawn(async move {
-        let service = &*service;
-        let sessions = &*sessions;
-        let socket = &socket;
-
-        // Accept messages future
-        let accept_future = accept_messages(service, sessions, socket);
-
-        // Forwarding future
-        let forward_future = forward_messages(socket, udp_forward_rx);
-
-        tokio::join!(accept_future, forward_future);
+    tokio::spawn(UdpTunnelFuture {
+        service,
+        sessions,
+        socket,
+        buffer: [0; u16::MAX as usize],
+        write_queue: VecDeque::new(),
+        rx: udp_forward_rx,
     });
 
     Ok(())
 }
 
-pub async fn forward_messages(socket: &UdpSocket, mut rx: UdpTunnelForwardRx) {
-    let mut futures = FuturesUnordered::new();
-    let mut futures = std::pin::pin!(futures);
+/// Future which handles processing UDP tunnel related tasks.
+pub struct UdpTunnelFuture {
+    /// Tunnel service for managing tunnels
+    service: Arc<TunnelService>,
 
-    poll_fn(|cx| {
-        // Poll new event execution
-        while let Poll::Ready(result) = rx.poll_recv(cx) {
-            let message = match result {
-                Some(value) => value,
+    /// Session service for verifying association tokens
+    sessions: Arc<Sessions>,
 
-                // All channels have been closed and the app is likely shutting down,
-                // finish the future and stop processing
-                None => return Poll::Ready(()),
-            };
+    /// Socket for sending and receiving messages
+    socket: UdpSocket,
 
-            // Push send future
-            futures.push(async move {
-                // Move required variables into the future
-                let message = message;
+    /// Buffer for reading received datagram packets
+    buffer: [u8; u16::MAX as usize],
 
-                // Send message
-                _ = socket
-                    .send_to(&message.buffer, message.target_address)
-                    .await;
-            });
-        }
+    /// Receiver for messages to write
+    rx: UdpTunnelForwardRx,
 
-        // Poll completions until no more ready
-        while let Poll::Ready(Some(_)) = futures.as_mut().poll_next(cx) {}
-
-        Poll::Pending
-    })
-    .await;
+    /// Queue for messages that need to be written
+    write_queue: VecDeque<UdpTunnelMessage>,
 }
 
-/// Reads inbound messages from the tunnel service
-pub async fn accept_messages(service: &TunnelService, sessions: &Sessions, socket: &UdpSocket) {
-    // Buffer to recv messages
-    let mut buffer = [0; u16::MAX as usize];
-
-    loop {
-        // Receive the message bytes
-        let (size, addr) = match socket.recv_from(&mut buffer).await {
+impl UdpTunnelFuture {
+    /// Poll receiving messages from the UDP socket
+    fn poll_recv_message(&mut self, cx: &mut std::task::Context<'_>) -> Poll<()> {
+        let mut buffer = ReadBuf::new(&mut self.buffer);
+        let addr = match ready!(self.socket.poll_recv_from(cx, &mut buffer)) {
             Ok(value) => value,
             Err(err) => {
                 if let Some(error_code) = err.raw_os_error() {
@@ -100,30 +82,92 @@ pub async fn accept_messages(service: &TunnelService, sessions: &Sessions, socke
                     // this happens when we tried to send a packet to a closed connection.
                     // error happens here instead of the sending portion for some reason
                     if error_code == 10054 {
-                        continue;
+                        return Poll::Ready(());
                     }
                 }
 
                 error!("failed to recv message: {err}");
-                continue;
+                return Poll::Ready(());
             }
         };
 
-        let buffer = &buffer[0..size];
+        let buffer = buffer.filled();
 
         // Deserialize the message
         let packet = match deserialize_message(buffer) {
             Ok(value) => value,
             Err(err) => {
                 error!("failed to deserialize packet: {}", err);
-                continue;
+                return Poll::Ready(());
             }
         };
 
         let tunnel_id = packet.header.tunnel_id;
 
         // Handle the message in its own task
-        handle_message(service, sessions, tunnel_id, packet.message, addr);
+        handle_message(
+            &self.service,
+            &self.sessions,
+            tunnel_id,
+            packet.message,
+            addr,
+        );
+        Poll::Ready(())
+    }
+
+    /// Poll messages from the send channel and place the send future onto the
+    /// futures list
+    fn poll_forward_message(&mut self, cx: &mut std::task::Context<'_>) -> Poll<()> {
+        let message = match ready!(self.rx.poll_recv(cx)) {
+            Some(value) => value,
+
+            // All channels have been closed and the app is likely shutting down,
+            // finish the future and stop processing
+            None => return Poll::Ready(()),
+        };
+
+        // Push message to the write queue
+        self.write_queue.push_back(message);
+
+        Poll::Ready(())
+    }
+
+    fn poll_write_message(&mut self, cx: &mut std::task::Context<'_>) -> Poll<()> {
+        let message = match self.write_queue.front() {
+            Some(value) => value,
+
+            // Waiting on a new packet to write
+            None => return Poll::Pending,
+        };
+
+        // Attempt to send the message
+        _ = ready!(self
+            .socket
+            .poll_send_to(cx, &message.buffer, message.target_address));
+
+        // Remove the sent message from the queue
+        _ = self.write_queue.pop_front();
+
+        Poll::Ready(())
+    }
+}
+
+impl Future for UdpTunnelFuture {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+        let this = self.get_mut();
+
+        // Poll incoming data
+        while this.poll_recv_message(cx).is_ready() {}
+
+        // Poll outgoing data
+        while this.poll_forward_message(cx).is_ready() {}
+
+        // Poll outgoing writes
+        while this.poll_write_message(cx).is_ready() {}
+
+        Poll::Pending
     }
 }
 
